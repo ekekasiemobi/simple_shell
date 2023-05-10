b2e5c0ce346e377209fcd3c9094d883d814dfad2
RealKingTino <tmowizzy@gmail.com>
2023-05-10 03:52:13 -0700

    header file

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <limits.h>
+#include <sys/wait.h>
+
+#endif
