c80b82cf78bb16d835fea060f706ed377aea0e95
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-05-10 10:25:39 -0700

    handled the path command

--- handle_path.c
+++ handle_path.c
@@ -16,42 +16,36 @@ char *get_path(char *first_command)
 	delimiter = ":";
 	cmd_path = getenv("PATH");
 
-	if (!cmd_path)
-		return (NULL);
-	cmd_path_copy = strdup(cmd_path);
-	length_user_command = strlen(first_command);
-
-	parse_token = strtok(cmd_path_copy, delimiter);
-
-	while (parse_token != NULL)
+	if (cmd_path)
 	{
-		dir_len = strlen(parse_token);
-		path_to_file_len = length_user_command + dir_len + 2;
-		/* added ↑ */
-		path_to_file = malloc(path_to_file_len);
+		cmd_path_copy = strdup(cmd_path);
+		length_user_command = s_len(first_command);
+		parse_token = strtok(cmd_path_copy, delimiter);
 
-		if (!path_to_file)
+		while (parse_token != NULL)
 		{
-			free(cmd_path_copy);
-			return (NULL);
+			dir_len = s_len(parse_token);
+			path_to_file_len = length_user_command + dir_len + 2;
+			/* added ↑ */
+			path_to_file = malloc(path_to_file_len /* 🤗 */);
+
+			s_copy(path_to_file, parse_token);
+			s_cat(path_to_file, "/");
+			s_cat(path_to_file, first_command);
+			s_cat(path_to_file, "\0");
+
+			if (stat(path_to_file, &path_test) == 0)
+			{
+				free(cmd_path_copy);
+				return (path_to_file);
+			}
+			free(path_to_file);
+			parse_token = strtok(NULL, delimiter);
 		}
-
-		strcpy(path_to_file, parse_token);
-		strcpy(path_to_file, "/");
-		strcpy(path_to_file, first_command);
-
-
-		if (stat(path_to_file, &path_test) == 0)
-		{
-			free(cmd_path_copy);
-			return (path_to_file);
-		}
-		free(path_to_file);
-		parse_token = strtok(NULL, delimiter);
 	}
-
 	free(cmd_path_copy);
 	if (stat(first_command, &path_test) == 0)
-		return (strdup(first_command));
+		return (first_command);
 	return (NULL);
 }
+
