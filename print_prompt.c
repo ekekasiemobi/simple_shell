a2a4e9db716e7e76079fdae8d28cdac9611b9d48
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-05-10 03:54:43 -0700

    created the shell prompt

---
+++ print_prompt.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * prompt_read - read prompts and displays inputs
+ * @fd_check: the number of byte read by getline
+ * Return: user_input
+ */
+
+char *prompt_read(ssize_t *fd_check)
+{
+	size_t byte_size = 0; /* assigned value to it */
+	char *display_prompt = "$ ";
+	char *user_input = NULL;
+
+	/* interactive_mode() */
+	if (isatty(STDIN_FILENO))
+	{
+		write(STDOUT_FILENO, display_prompt, 2);
+	}
+
+	*fd_check = getline(&user_input, &byte_size, stdin);
+
+	if (*fd_check == -1 && isatty(STDIN_FILENO))
+        {
+                write(STDOUT_FILENO, "\n", 1);
+                exit(EXIT_SUCCESS);
+        }
+        else if (*fd_check == -1)
+        {
+                exit(EXIT_SUCCESS);
+        }
+
+	/*write(STDOUT_FILENO, user_input, byte_size);*/
+	user_input[strcspn(user_input, "\n")] = '\0';
+
+	return (user_input);
+}
+
+
