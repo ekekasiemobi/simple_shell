53d5bb0e94ee53d57c7570af9fe0f16c30272562
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-05-10 04:26:44 -0700

    included the tokenizatin in shell.c

--- shell.c
+++ shell.c
@@ -10,11 +10,18 @@
 
 int main(int ac, char **argv)
 {
+	char **current_cmd = NULL;
+        int i, cmd_type = 0;
+        size_t n = 0;
+
 	char *user_input;
 	ssize_t fd_check;
 	char **args;
 	(void)ac;
 	(void)argv;
+
+	 signal(SIGINT, SIG_IGN);
+         shell_name = argv[0];
 
 	args = malloc(sizeof(char *) * 10);
 	/* TODO: you'll need a way to create a dynamic array */
@@ -27,5 +34,15 @@ int main(int ac, char **argv)
 		fd_check = 0;
 
 		user_input = prompt_read(&fd_check);
+		cmds = tokenizer(line, ";");
+                for (i = 0; cmds[i] != NULL; i++)
+                {
+                        current_cmd = tokenizer(cmds[i], " ");
+                        if (current_cmd[0] == NULL)
+                        {
+                                free(current_cmd);
+                                break;
+                        }
+                        cmd_type = determine_command_type(current_cmd[0]);
 	}
 }
