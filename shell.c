b2f3cc7e02da4d75efa9fc080276dc3d2b045c72
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-05-10 10:21:19 -0700

    add the execute function

--- shell.c
+++ shell.c
@@ -12,23 +12,26 @@ int main(int ac, char **argv)
 {
 	char *user_input;
 	ssize_t fd_check;
-	char *args[MAX_ARGS + 1];
-	void (*command)(void);
 	char **receive_argv;
-
+	char **args;
 	(void)ac;
 	(void)argv;
 
-	while (1)
+	args = malloc(sizeof(char *) * 10);
+	/* TODO: you'll need a way to create a dynamic array */
+
+	while (1 == 1)
 	{
+		void (*command)(void);
+
 		user_input = NULL;
 		fd_check = 0;
 
 		user_input = prompt_read(&fd_check);
 		/* parse user's command */
 		receive_argv = parse_user_input(user_input, args, fd_check);
-
 		/* execute user's command */
+
 		command = get_command(receive_argv[0]);
 		if (command != NULL)
 		{
@@ -36,8 +39,7 @@ int main(int ac, char **argv)
 			continue;
 		}
 		execute_command(receive_argv);
-
-		free(user_input);
-		free(receive_argv);
+		fflush(stdout);
 	}
 }
+
