71972b6b9f7e486c1363a4b85358935c4590a2b6
RealKingTino <tmowizzy@gmail.com>
2023-05-10 05:15:50 -0700

    updated  header file

--- main.h
+++ main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -15,14 +14,40 @@
 #include <limits.h>
 #include <sys/wait.h>
 
+char *get_path(char *first_command);
 
-int main(int argc __attribute__((unused)), char **argv);
+typedef int (*command_func)(void);
+
+typedef struct {
+    char *name;
+    int (*func)(void);
+} command_t;
 char *prompt_read(ssize_t *fd_check);
-int determine_command_type(char *command);
-void initializer(char **tokenized_cmd, int cmd_type);
-void s_cat(char *text_add, char *text);
-void s_copy(char *to, char *from);
-int s_len(char *string);
+void execute_command(char **receive_argv);
+char **parse_user_input(char *take_user_input, char **argv, ssize_t fd_check);
+char **tokenize_user_input(char *user_input);
+char *accept_user_input(void);
 
+void exit_program(void);
+void exit_shell(int status, char* message);
+int main(int ac, char **argv);
+typedef struct data_shell data_shell;
+void (*get_command(char *cmd))(void);
+
+/* strings related */
+int s_len(char *string);
+void s_copy(char *to, char *from);
+void s_cat(char *text_add, char *text);
+
+
+/* function related */
+char **handle_path(void);
+int check_ifCommand_exist(char *string);
+char **split_string(char *string);
+
+
+void print_error(char **av, char *err);
+int _errputs(char *str);
+void print_error(char **av, char *err);
 
 #endif
