86e254176ac6bbccc6bb8ccaf631c4a18d82b93d
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-05-14 17:29:29 -0700

    created free array function

--- shell.c
+++ shell.c
@@ -1,44 +1,62 @@
 #include "main.h"
 
+
 /**
  * main - shell clone
  * @ac: argument count
  * @argv: array of pointer to strings
  * Return: 1 or 0
  */
-
-
 int main(int ac, char **argv)
 {
-	char *user_input;
-	ssize_t fd_check;
-	char **receive_argv;
-	char **args;
-	(void)ac;
-	(void)argv;
+    char *user_input;
+    ssize_t fd_check;
+    char **receive_argv;
+    char **args = NULL;
+	int i;
+    (void)ac;
+    (void)argv;
 
-	args = malloc(sizeof(char *) * 10);
-	/* TODO: you'll need a way to create a dynamic array */
 
-	while (1 == 1)
+    /* TODO: you'll need a way to create a dynamic array */
+
+    while (1)
+    {
+        user_input = NULL;
+        fd_check = 0;
+
+        user_input = prompt_read(&fd_check);
+        /* parse user's command */
+        receive_argv = tokenization(user_input, args, fd_check);
+
+        if (strcmp(receive_argv[0], "exit") == 0)
+        {
+		free(user_input);
+            exit_shell(receive_argv);
+            continue;
+        }
+
+       i = execute_command(receive_argv);
+        fflush(stdout);
+	if (i == 1)
+		 print_error(receive_argv, "command not found\n");		
+	free(user_input);
+
+        free_array(receive_argv);
+
+    }
+	
+}
+
+void free_array(char **argv)
+{
+	char **temp = argv;
+
+
+	for (; *temp != NULL; temp++)
 	{
-		void (*command)(void);
-
-		user_input = NULL;
-		fd_check = 0;
-
-		user_input = prompt_read(&fd_check);
-		/* parse user's command */
-		receive_argv = parse_user_input(user_input, args, fd_check);
-		/* execute user's command */
-
-		command = get_command(receive_argv[0]);
-		if (command != NULL)
-		{
-			(*command)();
-			continue;
-		}
-		execute_command(receive_argv);
-		fflush(stdout);
+		free(*temp);
+		*temp = NULL;
 	}
+	free(argv);
 }
