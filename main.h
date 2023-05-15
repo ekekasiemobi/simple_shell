81e8a0531dc4c0c06ebbaec5fee0c32ef13822f5
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-05-14 17:27:59 -0700

    modified the header file

--- main.h
+++ main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -17,6 +19,11 @@
 
 #define MAX_ARGS 10
 
+/* constants for commands */
+#define EXT_COMMAND 1 /* external */
+#define INT_COMMAND 2 /* internal */
+#define PATH_COMMAND 3
+#define INVALID_COMMAND -1
 
 char *get_path(char *first_command);
 
@@ -26,14 +33,13 @@ typedef struct {
     char *name;
     int (*func)(void);
 } command_t;
+
 char *prompt_read(ssize_t *fd_check);
-void execute_command(char **receive_argv);
-char **parse_user_input(char *take_user_input, char **argv, ssize_t fd_check);
-char **tokenize_user_input(char *user_input);
+int execute_command(char **receive_argv);
+char **tokenization(char *take_user_input, char **argv, ssize_t fd_check);
+
 char *accept_user_input(void);
 
-void exit_program(void);
-void exit_shell(int status, char* message);
 int main(int ac, char **argv);
 typedef struct data_shell data_shell;
 void (*get_command(char *cmd))(void);
@@ -43,21 +49,24 @@ int s_len(char *string);
 void s_copy(char *to, char *from);
 void s_cat(char *text_add, char *text);
 
-
 /* function related */
 char **handle_path(void);
 int check_ifCommand_exist(char *string);
 char **split_string(char *string);
 
-
 void print_error(char **av, char *err);
 int _errputs(char *str);
 void print_error(char **av, char *err);
-
 
 char *_strchr(char *s, char c);
 int _strcspn(char *s, char *charset);
 int _strcmp(char *s, char *c);
 
+void exit_shell(char **tokenized_command);
+void execute_cmd(char **command, int command_type);
+int determine_command_type(char *command);
+ int _atoi(char *s);
+
+ void free_array(char **argv);
 #endif
 
