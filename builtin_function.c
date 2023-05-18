424f87890006def7fe139ef0f8e295cb29543514
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-05-18 08:35:52 -0700

    fixed all betty error in the exit_shell function

--- builtin_function.c
+++ builtin_function.c
@@ -8,43 +8,46 @@
  */
 void exit_shell(char **tokenized_command)
 {
-    char *shell_name = NULL;
-    char *line = NULL;
-    char **cmds = NULL;
-    int status = 0;
-    int num_token = 0, arg;
-    const char *error_message = "$: too many arguments\n";
+	char *shell_name = NULL;
+	char *line = NULL;
+	char **cmds = NULL;
+	int status = 0;
+	int num_token = 0, arg;
+	const char *error_message = "$: too many arguments\n";
 
-    for (; tokenized_command[num_token] != NULL; num_token++)
-        ;
+	for (; tokenized_command[num_token] != NULL; num_token++)
+		;
+	{
 
-    if (num_token == 1)
-    {
-        free_array(tokenized_command);
-        free(line);
-        free(cmds);
-        exit(status);
-    }
-    else if (num_token == 2)
-    {
-        arg = _atoi(tokenized_command[1]);
+		if (num_token == 1)
+		{
+			free_array(tokenized_command);
+			free(line);
+			free(cmds);
+			exit(status);
+		}
+		else if (num_token == 2)
+		{
+			arg = _atoi(tokenized_command[1]);
+		
 
-        if (arg == -1)
-        {
-            write(STDERR_FILENO, shell_name, strlen(shell_name));
-            write(STDERR_FILENO, ": 1: exit: error ", strlen(": 1: exit: error "));
-            write(STDERR_FILENO, tokenized_command[1], strlen(tokenized_command[1]));
-            write(STDERR_FILENO, "\n", 1);
-            status = 2;
-        }
-        else
-        {
-            free(line);
-            free(tokenized_command);
-            free(cmds);
-            exit(arg);
-        }
-    }
-    else
-        write(STDERR_FILENO, error_message, strlen(error_message));
+			if (arg == -1)
+			{
+				write(STDERR_FILENO, shell_name, strlen(shell_name));
+				write(STDERR_FILENO, ": 1: exit: error ", strlen(": 1: exit: error "));
+				write(STDERR_FILENO, tokenized_command[1], strlen(tokenized_command[1]));
+				write(STDERR_FILENO, "\n", 1);
+				status = 2;
+			}
+			else
+			{
+				free(line);
+				free(tokenized_command);
+				free(cmds);
+				exit(arg);
+			}
+		}
+		else
+		write(STDERR_FILENO, error_message, strlen(error_message));
+	}
 }
