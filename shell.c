0bea63808fa3f361d6d08b5e320662b70f7d3453
RealKingTino <tmowizzy@gmail.com>
2023-05-18 06:10:40 -0700

    implement getline in shell.c

--- shell.c
+++ shell.c
@@ -1,6 +1,5 @@
+#include <stdio.h>
 #include "main.h"
-
-
 /**
  * main - shell clone
  * @ac: argument count
@@ -9,49 +8,56 @@
  */
 int main(int ac, char **argv)
 {
-    char *user_input;
-    ssize_t fd_check;
-    char **receive_argv;
-    char **args = NULL;
+	char *user_input;
+	ssize_t fd_check;
+	char **receive_argv;
 	int i;
-    (void)ac;
-    (void)argv;
+	int execution_status;
+	(void)ac;
+	(void)argv;
 
-
-    /* TODO: you'll need a way to create a dynamic array */
-
-    while (1)
-    {
-        user_input = NULL;
-        fd_check = 0;
-
-        user_input = prompt_read(&fd_check);
-        /* parse user's command */
-        receive_argv = tokenization(user_input, args, fd_check);
-
-        if (strcmp(receive_argv[0], "exit") == 0)
-        {
+	while (1)
+	{
+		user_input = NULL;
+		fd_check = 0;
+		user_input = prompt_read(&fd_check);
+		/* parse user's command */
+		receive_argv = tokenization(user_input, receive_argv, fd_check);
+		if (receive_argv[0] == NULL)
+		{
+			continue;
+		}
+		if (strcmp(receive_argv[0], "exit") == 0)
+		{			free(user_input);
+			exit_shell(receive_argv);
+			continue;
+		}
+		i = handle_env_commands(receive_argv);
+		if (i != 0)
+		{
+			free(user_input);
+			free_array(receive_argv);
+			continue;
+		}
+		execution_status = execute_command(receive_argv);
+		if (execution_status == 1)
+		{
+			print_error(receive_argv, "Command not found\n");
+		}
 		free(user_input);
-            exit_shell(receive_argv);
-            continue;
-        }
-
-       i = execute_command(receive_argv);
-        fflush(stdout);
-	if (i == 1)
-		 print_error(receive_argv, "command not found\n");		
-	free(user_input);
-
-        free_array(receive_argv);
-
-    }
-	
+		free_array(receive_argv);
+	}
+	return (0);
 }
 
+/**
+ * free_array - Frees a dynamically allocated array of strings.
+ * @argv: The array of strings to be freed.
+ *
+ */
 void free_array(char **argv)
 {
 	char **temp = argv;
-
 
 	for (; *temp != NULL; temp++)
 	{
