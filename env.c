a77c1bf27fea41268359b81675741308c21e6307
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-05-18 05:53:21 -0700

    implemented the env function

---
+++ env.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _env - lists the enviromental variables
+ *
+ * @datash: the date.
+ * Return: return 1 when successful
+ */
+int _env(data_shell *datash)
+{
+	int i, j;
+
+	for (i = 0; datash->_environ[i]; i++)
+	{
+
+		for (j = 0; datash->_environ[i][j]; j++)
+			;
+
+		write(STDOUT_FILENO, datash->_environ[i], j);
+		write(STDOUT_FILENO, "\n", 1);
+	}
+	datash->status = 0;
+
+	return (1);
+}
