222cd11798512c68ac222f49335c7a1923067ada
RealKingTino <tmowizzy@gmail.com>
2023-05-23 12:53:29 -0700

    builtin cd command

--- functions.c
+++ functions.c
@@ -48,3 +48,96 @@ void display_exit_error(char *shell_name, char *arg)
 	write(STDERR_FILENO, arg, strlen(arg));
 	write(STDERR_FILENO, "\n", 1);
 }
+/**
+ * cd_check_args - Check and process the arguments for the cd command
+ *
+ * @args: The command arguments
+ * @new_dir: Pointer to store the new directory path
+ *
+ * Return: 0 on success, -1 on failure
+ */
+int cd_check_args(char **args, char **new_dir)
+{
+	if (args[1] == NULL || _strcmp(args[1], "~") == 0)
+		*new_dir = _getenv("HOME");
+	else if (_strcmp(args[1], "-") == 0)
+	{
+		*new_dir = _getenv("OLDPWD");
+		if (*new_dir == NULL)
+		{
+			write(STDERR_FILENO, "cd: OLDPWD not set\n", 19);
+			return (-1);
+		}
+	}
+	else if (args[1][0] == '$')
+	{
+		char *env_var = _getenv(args[1] + 1);
+
+		if (env_var == NULL)
+		{
+			write(STDERR_FILENO, "cd: No such variable\n", 21);
+			return (-1);
+		}
+		*new_dir = env_var;
+	}
+	else
+		*new_dir = args[1];
+
+	if (*new_dir == NULL)
+	{
+		print_error(args, "HOME not set\n");
+		return (-1);
+	}
+
+	return (0);
+}
+/**
+ * cd_command - Change the current working directory
+ *
+ * @args: The command arguments
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int cd_command(char **args)
+{
+	char *new_dir;
+	char old_dir[PATH_MAX];
+	int result;
+
+	if (cd_check_args(args, &new_dir) == -1)
+		return (-1);
+
+	if (getcwd(old_dir, sizeof(old_dir)) == NULL)
+	{
+		print_error(args, "getcwd error\n");
+		return (-1);
+	}
+
+	result = chdir(new_dir);
+	if (result != 0)
+	{
+		print_error(args, "chdir error\n");
+		return (-1);
+	}
+	if (setenv("OLDPWD", old_dir, 1) == -1)
+	{
+		print_error(args, "setenv error\n");
+		return (-1);
+	}
+	if (_strcmp(args[0], "cd") == 0 || _strcmp(args[0], "PWD") == 0)
+	{
+		if (getcwd(old_dir, sizeof(old_dir)) == NULL)
+		{
+			print_error(args, "getcwd error\n");
+			return (-1);
+		}
+
+		if (setenv("PWD", old_dir, 1) == -1)
+		{
+			print_error(args, "setenv error\n");
+			return (-1);
+		}
+	}
+
+	return (1);
+}
